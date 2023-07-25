a804cf59a1d70b7f9aac6a01474e2a65d6987b0d
Daniel Bonn <danieltbonn@gmail.com>
2023-07-24 21:10:03 -0500

    added functionality for getting jokes based on number from dice roll

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,8 +1,11 @@
+// keys to both of our APIs
+var diceUrl = 'https://roll-dice1.p.rapidapi.com/rollDice/?rapidapi-key=a652a06e19msh2f6363022ab93a8p1108bejsn7f3bb112893f';
+var jokeUrl = 'https://geek-jokes.sameerkumar.website/api?format=json';
 
+// gets the dice roll from our dice API
+function getDiceApi(requestUrl) {
 
-function getApi(requestUrl) {
-
-    fetch(requestUrl) 
+   fetch(requestUrl) 
         .then(function(response) {
             
             console.log(response);
@@ -10,13 +13,30 @@ function getApi(requestUrl) {
         })
         .then(function(data) {
             console.log(data);
+            getJokeApi(jokeUrl, 5)
+            return data;
         })
     
 }
 
-var diceUrl = 'https://roll-dice1.p.rapidapi.com/rollDice/?rapidapi-key=a652a06e19msh2f6363022ab93a8p1108bejsn7f3bb112893f';
-var jokeUrl = 'https://geek-jokes.sameerkumar.website/api?format=json';
+// gets the jokes per number of roll from the jokes api
+function getJokeApi(requestUrl, roll) {
 
-getApi(diceUrl);
-getApi(jokeUrl);
+    for (i = 0; i < roll; i++) {
 
+        fetch(requestUrl) 
+        .then(function(response) {
+            
+            console.log(response);
+            return response.json();;
+        })
+        .then(function(data) {
+            console.log(data);
+            return data;
+        })
+    }
+        
+     
+ }
+
+getDiceApi(diceUrl);
