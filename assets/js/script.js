1bad42e9a51fdc8ad3bd2306dbe7960e4d76738e
Kiara Johnson <kiarajohnson@kiaras-mbp.lan>
2023-07-26 20:46:42 -0500

    pushed localstorage back

--- assets/js/script.js
+++ assets/js/script.js
@@ -35,3 +35,23 @@ function getJokeApi(requestUrl, roll) {
  }
 
 getDiceApi(diceUrl);
+
+// Creates the jokes object
+let jokes = {
+    jokeOne: "one",
+    jokeTwo: "two"
+}
+
+// Converts jokes to string (this step might not be neccessary)
+let jokesString = JSON.stringify(jokes);
+
+// Stores jokes in local storage
+localStorage.setItem("jokes", jokesString);
+console.log(jokesString);
+
+// Creates a variable that converts string back to an object
+let jokesDeString = JSON.parse(localStorage.getItem("jokes"));
+console.log(jokesDeString);
+
+// Delete button will remove from local storage
+localStorage.removeItem();
