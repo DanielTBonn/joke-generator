d778065c63c0aa78b213f46c4fcb9170601ddd86
Daniel Bonn <danieltbonn@gmail.com>
2023-08-01 20:17:25 -0500

    added a message stating when there are no saved jokes on the page

--- assets/js/savejokes.js
+++ assets/js/savejokes.js
@@ -3,28 +3,40 @@ function displayJokes(){
 
     var listEl = $("#saved-jokes-list");
     var listItemsInStorage = JSON.parse(localStorage.getItem("jokes"));
-    
-    for (let i = 0; i < listItemsInStorage.length; i++) {
-        var listItemEl = $('<li>');
-        var deleteBtn = $('<button id="delBtn"style="background-color:#33691e; width:100px;"><a class="waves-effect waves-teal btn-flat lime-text text-lighten-2" style="background-color:#33691e">Delete</a></button>');
-        var text = listItemsInStorage[i]
-        deleteBtn.attr('data-text',text);
-        listItemEl.text(text);
-        listItemEl.append(deleteBtn);
-        listEl.append(listItemEl);
 
-        deleteBtn.on('click', function(){
-            var listItemsInStorage = JSON.parse(localStorage.getItem("jokes"));
-            var data = $(this).attr('data-text')
-            var filteredListItems = listItemsInStorage.filter((item, index) => {
-                console.log({data, item})
-              return  data !== item
-            });
-            localStorage.setItem('jokes',JSON.stringify(filteredListItems));
-            $(this).parent().remove();
-        
-        })
+    if (!listItemsInStorage) {
+        // console.log(listItemsInStorage)
+        var nothingSection = $('#nothing-here');
+        var nothingHere = $("<h2 class='pink-text text-accent-3'>You have no saved jokes.</h2>");
+        nothingSection.append(nothingHere);
+        console.log("hi")
+
+    } else {
+        for (let i = 0; i < listItemsInStorage.length; i++) {
+            var listItemEl = $('<li>');
+            var deleteBtn = $('<button id="delBtn"style="background-color:#33691e; width:100px;"><a class="waves-effect waves-teal btn-flat lime-text text-lighten-2" style="background-color:#33691e">Delete</a></button>');
+            var text = listItemsInStorage[i]
+            deleteBtn.attr('data-text',text);
+            listItemEl.text(text);
+            listItemEl.append(deleteBtn);
+            listEl.append(listItemEl);
+    
+            deleteBtn.on('click', function(){
+                var listItemsInStorage = JSON.parse(localStorage.getItem("jokes"));
+                var data = $(this).attr('data-text')
+                var filteredListItems = listItemsInStorage.filter((item, index) => {
+                    console.log({data, item})
+                  return  data !== item
+                });
+                localStorage.setItem('jokes',JSON.stringify(filteredListItems));
+                $(this).parent().remove();
+            
+            })
+        }
+
+    
     }
+
 }
     
 displayJokes();
